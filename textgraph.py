cf857ad0e43f3e97959973374c857c9e59bb111a
Issah45 <90643234+Issah45@users.noreply.github.com>
2024-02-24 22:36:51 +0800

    enter the square world

--- textgraph.py
+++ textgraph.py
@@ -1,9 +1,29 @@
 import os, time, termcolor
 
-def display(content, color="white"):
+def display(content, color="white", end="\n"):
 	c = termcolor.colored(content, color)
-	print(c)
+	print(c, end=end)
 
 def update(delay):
 	time.sleep(delay)
 	os.system("clear")
+
+def rectangle(x, y, width, height, color="white"):
+	a = []
+	b = []
+	
+	for i in range(width):
+		a.append("▇")
+	for i in range(width):
+		b.append(a)
+	
+	for i in range(x):
+		print(" ")
+	
+	for i in range(y):
+		print()
+	
+	for c in b:
+		for d in c:
+			display(d, color=color, end="")
+		print()
